aeeb7ae3c670da10863c73b5302bc1e2f2923163
Nathaniel J. Smith <njs@anthropic.com>
2023-04-24 11:05:16 -0700

    pin libunwind

--- prep-manylinux-container.sh
+++ prep-manylinux-container.sh
@@ -4,8 +4,16 @@ set -exo pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+# We could get libunwind by doing
+#
+#   yum install -y libunwind-devel
+#
+# but that failed b/c the static library wasn't built with -fPIC, and that's required
+# because our final binary is a PIE, so everything in it has to be PIC. So build our own
+# libunwind that we can force to use -fPIC.
 cd /
-git clone https://github.com/libunwind/libunwind
+curl https://github.com/libunwind/libunwind/archive/refs/tags/v1.6.2.tar.gz  -o libunwind.tar.gz
+tar xvf libunwind.tar.gz
 cd libunwind
 autoreconf -i
 ./configure CFLAGS="-fPIC"
