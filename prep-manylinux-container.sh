37cf1d160132383c97a632942cb208d015afa31f
Nathaniel J. Smith <njs@anthropic.com>
2023-04-27 13:22:19 -0700

    libunwind experiment

--- prep-manylinux-container.sh
+++ prep-manylinux-container.sh
@@ -16,6 +16,6 @@ curl -L https://github.com/libunwind/libunwind/archive/refs/tags/v1.6.2.tar.gz  
 tar xvf libunwind.tar.gz
 cd libunwind-*/
 autoreconf -i
-./configure CFLAGS="-fPIC"
+./configure CFLAGS="-fPIC" --disable-shared
 make -j3
 make install
