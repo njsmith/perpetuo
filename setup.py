14239dc866cf3332a3cba9ae89f9f6431715228c
Nathaniel J. Smith <njs@anthropic.com>
2023-04-26 20:55:38 -0700

    Hack setup.py to build abi3 wheels

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustBin, RustExtension, Strip
+from wheel.bdist_wheel import bdist_wheel
+
+
+# https://github.com/joerick/python-abi3-package-sample/blob/main/setup.py
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.9
+            return "cp39", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
     name="perpetuo",
@@ -12,6 +26,7 @@ setup(
         ),
         RustBin("perpetuo", strip=Strip.Debug),
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
     package_dir={"": "python"},
     packages=["perpetuo"],
     package_data={"perpetuo": ["py.typed", "__init__.pyi"]},
