a1b9ab81248eec4137a474c14622ea5f11f3c7cf
Nathaniel J. Smith <njs@anthropic.com>
2024-12-12 15:41:17 -0800

    Emit one json record per stall report

--- src/log.rs
+++ src/log.rs
@@ -1,6 +1,6 @@
+use py_spy::StackTrace;
 use serde::Serialize;
 use std::collections::HashMap;
-use py_spy::StackTrace;
 
 #[derive(Serialize, Debug, Clone, Copy)]
 #[serde(rename_all = "UPPERCASE")]
@@ -14,7 +14,7 @@ pub enum Severity {
 }
 
 #[derive(Serialize)]
-struct StackFrame {
+pub struct StackFrame {
     name: String,
     filename: String,
     line: i32,
@@ -28,58 +28,15 @@ struct LocalVariable {
 }
 
 #[derive(Serialize)]
-struct LogEntry<'a> {
-    severity: Severity,
-    message: &'a str,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    additional_info: Option<&'a HashMap<String, String>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    traceback: Option<&'a Vec<StackFrame>>,
+pub struct StallDetails {
+    pub length_ms: f64,
+    pub relevant_traces: Vec<Vec<StackFrame>>,
+    pub other_traces: Vec<Vec<StackFrame>>,
+    pub cmdline: Vec<String>,
+    pub rate_limited: bool,
 }
 
-pub fn log(severity: Severity, message: &str, additional_info: Option<&HashMap<String, String>>, json_mode: bool) {
-    log_with_traceback(severity, message, additional_info, None, json_mode);
-}
-
-fn log_with_traceback(severity: Severity, message: &str, additional_info: Option<&HashMap<String, String>>, traceback: Option<&Vec<StackFrame>>, json_mode: bool) {
-    if json_mode {
-        let entry = LogEntry {
-            severity,
-            message,
-            additional_info,
-            traceback,
-        };
-
-        if let Ok(json) = serde_json::to_string(&entry) {
-            eprintln!("{}", json);
-        }
-    } else {
-        let severity_str = format!("{:?}", severity);
-        eprintln!("[{}] {}", severity_str, message);
-        if let Some(info) = additional_info {
-            for (key, value) in info {
-                eprintln!("  {}: {}", key, value);
-            }
-        }
-        if let Some(trace) = traceback {
-            eprintln!("Traceback:");
-            for frame in trace {
-                eprintln!("        {} ({}:{})", frame.name, frame.filename, frame.line);
-                if let Some(locals) = &frame.locals {
-                    for local in locals {
-                        eprintln!(
-                            "            {} = {}",
-                            local.name,
-                            local.value
-                        );
-                    }
-                }
-            }
-        }
-    }
-}
-
-pub fn dump_stacktrace(trace: &StackTrace, json_mode: bool) {
+pub fn convert_stack(trace: &StackTrace) -> Vec<StackFrame> {
     let mut frames = Vec::new();
     for frame in trace.frames.iter().rev() {
         let mut locals = None;
@@ -102,20 +59,82 @@ pub fn dump_stacktrace(trace: &StackTrace, json_mode: bool) {
             locals,
         });
     }
+    frames
+}
 
-    let mut additional_info = HashMap::new();
-    additional_info.insert("thread_id".to_string(), format!("{:x}", trace.thread_id));
-    additional_info.insert("status".to_string(), trace.status_str().to_string());
-    additional_info.insert("owns_gil".to_string(), trace.owns_gil.to_string());
+fn print_stack(trace: &[StackFrame]) {
+    for frame in trace {
+        eprintln!("        {} ({}:{})", frame.name, frame.filename, frame.line);
+        if let Some(locals) = &frame.locals {
+            for local in locals {
+                eprintln!("            {} = {}", local.name, local.value);
+            }
+        }
+    }
+}
 
-    log_with_traceback(
-        Severity::Info,
-        &format!(
-            "Thread {:x}",
-            trace.thread_id,
-        ),
-        Some(&additional_info),
-        Some(&frames),
-        json_mode,
-    );
+#[derive(Serialize)]
+struct LogEntry<'a> {
+    severity: Severity,
+    message: &'a str,
+    additional_info: &'a HashMap<String, String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    stall_details: Option<&'a StallDetails>,
+}
+
+pub fn log(
+    severity: Severity,
+    message: &str,
+    additional_info: &HashMap<String, String>,
+    json_mode: bool,
+) {
+    log_with_details(severity, message, additional_info, None, json_mode);
+}
+
+pub fn log_with_details(
+    severity: Severity,
+    message: &str,
+    additional_info: &HashMap<String, String>,
+    stall_details: Option<&StallDetails>,
+    json_mode: bool,
+) {
+    if json_mode {
+        let entry = LogEntry {
+            severity,
+            message,
+            additional_info,
+            stall_details,
+        };
+
+        if let Ok(json) = serde_json::to_string(&entry) {
+            eprintln!("{}", json);
+        }
+    } else {
+        let severity_str = format!("{:?}", severity);
+        let details_formatted = additional_info
+            .iter()
+            .map(|(k, v)| format!("{k}: {v}"))
+            .collect::<Vec<_>>()
+            .join(", ");
+        eprintln!("[{}] {} ({})", severity_str, message, details_formatted);
+        if let Some(details) = stall_details {
+            eprintln!("  Duration (so far): {} ms", details.length_ms);
+            eprintln!("  Command line: {}", details.cmdline.join(" "));
+            if details.rate_limited {
+                eprintln!("  -- (no traceback because of rate limiting) --");
+            }
+            if !details.relevant_traces.is_empty() {
+                eprintln!("  -- This thread is probably responsible --");
+                for trace in &details.relevant_traces {
+                    print_stack(trace);
+                }
+            }
+            if !details.other_traces.is_empty() {
+                eprintln!("  -- Other threads (probably not responsible) --");
+                for trace in &details.other_traces {
+                    print_stack(trace);
+                }
+            }
+        }
+    }
 }
